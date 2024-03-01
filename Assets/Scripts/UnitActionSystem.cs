8972a20daaa99f450150f18036ff691a21440c25
Angel Bowers <ANGELLBOWERS@GMAIL.COM>
2024-03-01 07:21:50 -0600

    Event system now turns unit selected visuals on and off accordingly

--- Assets/Scripts/UnitActionSystem.cs
+++ Assets/Scripts/UnitActionSystem.cs
@@ -5,16 +5,31 @@ using System;
 
 public class UnitActionSystem : MonoBehaviour
 {
+    public static UnitActionSystem Instance { get; private set; }
+
     [SerializeField] private Unit selectedUnit;
     [SerializeField] private LayerMask unitLayerMask;
 
     public event EventHandler OnSelectedUnitChanged;
+
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Debug.LogError("There is more than one UnitActionSystem! " + transform + " - " + Instance);
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+    }
 
     private void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
             if (TryHandleUnitSelection()) { return; }
+
             selectedUnit.Move(MouseWorld.GetPosition());
         }
     }
--- Assets/Scripts/UnitSelectedVisual.cs
+++ Assets/Scripts/UnitSelectedVisual.cs
@@ -11,4 +11,33 @@ public class UnitSelectedVisual : MonoBehaviour
     {
         meshRenderer = GetComponent<MeshRenderer>();
     }
+
+    private void Start()
+    {
+        UnitActionSystem.Instance.OnSelectedUnitChanged += UnitActionSystem_OnSelectedUnitChanged;
+
+        UpdateVisual();
+    }
+
+    private void OnDisable()
+    {
+        UnitActionSystem.Instance.OnSelectedUnitChanged -= UnitActionSystem_OnSelectedUnitChanged;
+    }
+
+    private void UnitActionSystem_OnSelectedUnitChanged(object sender, System.EventArgs empty)
+    {
+        UpdateVisual();
+    }
+
+    private void UpdateVisual()
+    {
+        if (UnitActionSystem.Instance.GetSelectedUnit() == unit)
+        {
+            meshRenderer.enabled = true;
+        }
+        else
+        {
+            meshRenderer.enabled = false;
+        }
+    }
 }
