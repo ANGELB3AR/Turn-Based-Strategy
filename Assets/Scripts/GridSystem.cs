6b52d180dd3b41d46b39087689fe6defad4f5bd1
Angel Bowers <ANGELLBOWERS@GMAIL.COM>
2024-03-01 07:43:10 -0600

    Started building GridSystem

---
+++ Assets/Scripts/GridSystem.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridSystem
+{
+    private int width;
+    private int length;
+    private float cellSize;
+
+    public GridSystem(int width, int length, float cellSize)
+    {
+        this.width = width;
+        this.length = length;
+        this.cellSize = cellSize;
+
+        for (int x = 0; x < width; x++)
+        {
+            for (int z = 0; z < length; z++)
+            {
+                Debug.DrawLine(GetWorldPosition(x, z), GetWorldPosition(x, z) + Vector3.right * .2f, Color.white, 1000);
+            }
+        }
+    }
+
+    public Vector3 GetWorldPosition(int x, int z)
+    {
+        return new Vector3(x, 0, z) * cellSize;
+    }
+}
